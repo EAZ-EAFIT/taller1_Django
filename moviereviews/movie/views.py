4c425ee220c02e07749424fa78d3bdca7ff77dd5
Esteban-Alvarez <84643021+Esteban-Alvarez@users.noreply.github.com>
2024-08-01 11:43:40 -0500

    visualizacion de películas con estilo

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from movie.models import Movie
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name':'Esteban Alvarez'})
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    if searchTerm:
+        movies = movies.filter(title__icontains=searchTerm)
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
 
 def about(request):
     return render(request, 'about.html')
